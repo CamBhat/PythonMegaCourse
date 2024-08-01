2a1037cc8696dd7815be20539fa2caed46b65e0d
Cam Bhatnagar <cjbhatnagar@icloud.com>
2024-08-01 15:16:41 -0400

    Day 5

--- App1-ToDo/main.py
+++ App1-ToDo/main.py
@@ -1,18 +1,28 @@
+# Initial list to be manipulated
 todos = ['Test1', 'Test2', 'Test3']
 
 while True:
-    user_action = input("Type add, show, edit, delete, or exit: ")
+    
+    # Actions a user can take
+    user_action = input("Type add, show, edit, complete, or exit: ")
 
+    # Strips spaces
     user_action = user_action.strip()
 
     # Does different actions based on user's input
     match user_action:
+        
+        # Adds item to To-Do list
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
+            
+        # Shows To-Do list
         case 'show':
-            for todo in todos:
-                print(todos.index(todo) + 1, "-", todo)
+            for index, todo in enumerate(todos):
+                print(f"{index + 1} - {todo}")
+                
+        # Edits an item in the To-Do list
         case 'edit':
             number = int(input("Number of todo to edit: "))
             
@@ -22,17 +32,24 @@ while True:
                 todos[number - 1] = new 
             else:
                 print("Index of todo not found")
-        case 'delete':
-            number = int(input("Number of todo to delete: "))
+                
+        # Completes and removes an item from To-Do list
+        case 'complete':
+            number = int(input("Number of todo to complete: "))
             
             if 1 <= number <= len(todos):
-                print(todos[number - 1], "has been delete")
-                del todos[number - 1]
+                print(todos[number - 1], "has been completed")
+                todos.pop(number - 1)
             else:
                 print("Index of todo not found")
+                
+        # Ends the program
         case 'exit':
             break
+        
+        # Edge case of unknown commands
         case foo:
             print("Unknown command")
 
+# Exit print
 print("Bye!")
